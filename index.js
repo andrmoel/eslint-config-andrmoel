9a4250d06df6c7f4b633de2b87fc1dfb9896a353
Andreas Moeller <andrmoel@gmail.com>
2020-02-27 20:33:27 +0100

    chore(eslint): initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+module.exports = {
+    'settings': {
+        'react': {
+            'createClass': 'createReactClass',
+            'pragma': 'React',
+            'version': '16',
+        },
+    },
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'ecmaVersion': 6,
+        'sourceType': 'module',
+        'ecmaFeatures': {
+            'jsx': true,
+        },
+    },
+    'extends': [
+        'google',
+        'plugin:react/recommended',
+    ],
+    'plugins': [
+        'babel',
+    ],
+    'rules': {
+        'indent': [
+            'error', 4, {
+                'SwitchCase': 1,
+            },
+        ],
+        'max-len': ['error', 120, 4],
+        'space-infix-ops': [
+            'error', {
+                'int32Hint': false,
+            },
+        ],
+        'padded-blocks': ['error', 'never'],
+        'prefer-arrow-callback': ['error'],
+        'no-multiple-empty-lines': [
+            'error', {
+                'max': 1,
+                'maxBOF': 0,
+                'maxEOF': 0,
+            },
+        ],
+        'lines-between-class-members': ['error', 'always'],
+        'newline-before-return': 'error',
+        'operator-linebreak': ['error', 'before'],
+        'eqeqeq': ['error'],
+        'require-jsdoc': [
+            'error', {
+                'require': {
+                    'FunctionDeclaration': false,
+                    'MethodDefinition': false,
+                    'ClassDeclaration': false,
+                    'ArrowFunctionExpression': false,
+                    'FunctionExpression': false,
+                },
+            },
+        ],
+        'valid-jsdoc': [
+            'error', {
+                'prefer': {
+                    'return': 'returns',
+                },
+                'requireReturn': false,
+                'requireReturnDescription': false,
+                'requireParamDescription': false,
+            },
+        ],
+        'no-extra-semi': 'error',
+        'no-invalid-this': 0,
+        'jsx-quotes': ['error', 'prefer-double'],
+        'babel/no-invalid-this': 1,
+        'react/display-name': 0,
+        'react/no-unescaped-entities': [
+            'error', {
+                'forbid': ['>', '}'],
+            },
+        ],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-max-props-per-line': ['error', {'maximum': 1, 'when': 'multiline'}],
+        'react/jsx-first-prop-new-line': ['error', 'multiline'],
+        'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+        'react/jsx-wrap-multilines': [
+            'error', {
+                'declaration': 'parens-new-line',
+                'assignment': 'parens-new-line',
+                'return': 'parens-new-line',
+                'arrow': 'parens-new-line',
+                'condition': 'parens-new-line',
+                'logical': 'parens-new-line',
+                'prop': 'parens-new-line',
+            },
+        ],
+    },
+};
