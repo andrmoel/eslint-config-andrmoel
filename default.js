258ab8f84e164387f114ed5f39afe94fae19ec63
Andreas Moeller <andrmoel@gmail.com>
2021-03-22 21:23:42 +0100

    feat(eslint): remove newlines in import order

--- default.js
+++ default.js
@@ -72,6 +72,6 @@ module.exports = {
         'no-extra-semi': 'error',
         'no-invalid-this': 0,
         'babel/no-invalid-this': 1,
-        'import/order': ['error', {'newlines-between': 'always'}],
+        'import/order': ['error'],
     },
 };
